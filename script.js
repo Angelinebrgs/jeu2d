cc25c206ade296e0d4c248692aac5e72ba1e2934
Angéline <112820179+Angelinebrgs@users.noreply.github.com>
2023-07-17 12:09:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load',function(){
+    //setup du canvas
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+});
