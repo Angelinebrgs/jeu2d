205388eba1ec3f66556d54e790d51ae92aa1b886
Angéline <titou.borges@gmail.com>
2023-07-24 14:06:17 +0200

    24/04/23 sauvegarde animation du player

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ window.addEventListener('load',function(){
     //setup du canvas
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1500;
+    canvas.width = 500;
     canvas.height = 500;
-
+    //création des mes objet
     class InputHandler {
-
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', function(){
+                
+            });
+        }
     }
     class Projectile {
 
@@ -15,9 +20,54 @@ window.addEventListener('load',function(){
 
     }
     class Player {
-
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
 
     }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context)
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    //animation loop
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
